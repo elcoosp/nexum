f32bc22b29a4963b8c4147c74171881e61c2de40
elcoosp <elcoosp@gmail.com>
2026-04-18 15:10:07 +0200

    feat(floem): complete adapter with macOS/Win/Linux registration

--- examples/floem-basic/src/main.rs
+++ examples/floem-basic/src/main.rs
@@ -1,9 +1,9 @@
-use floem::prelude::{SignalGet, SignalUpdate};
+use floem::prelude::*;
 use floem::reactive::create_rw_signal;
 use floem::views::{label, v_stack};
 use floem::View;
 use nexum_core::Config;
-use nexum_floem::create_deep_link_listener;
+use nexum_floem::setup;
 
 fn app_view() -> impl View {
     let deep_link_urls = create_rw_signal(String::from("No deep link received"));
@@ -11,12 +11,12 @@ fn app_view() -> impl View {
         schemes: vec!["floem".to_string()],
         app_links: vec![],
     };
-    let rx = create_deep_link_listener(config);
+    let handle = setup(config);
 
     let urls_signal = deep_link_urls;
     std::thread::spawn(move || {
-        while let Ok(urls) = rx.recv_blocking() {
-            urls_signal.set(format!("Last URL: {:?}", urls));
+        while let Ok(url) = handle.recv_blocking() {
+            urls_signal.set(format!("Last URL: {}", url));
         }
     });
 
